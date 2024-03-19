59a45b3e59bee59b2f51a0cb7fdd6820de8d956b
theavannthon <153904284+theavannthon@users.noreply.github.com>
2024-03-19 18:43:34 +0700

    save all

--- Library/obj/Debug/net8.0/Library.AssemblyInfo.cs
+++ Library/obj/Debug/net8.0/Library.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+284772580e29708092190b94924c834a4c184682")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b53bedf37e35a38b806b8fb00ecd7064f333d66")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
